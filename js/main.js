5e8640a43ffc09d0fe2e538ab94e92c5b0a9eba0
vertina61 <natalya.vertieva@mail.ru>
2022-02-24 14:45:31 +0300

    Использовали шаблонные строки+передала параметры функциям

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let returnRandomInteger = function (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-       let rez=Math.floor(Math.random() * (max - min + 1)) + min
-      if(min>0) {return rez + ' целое число из диапазона от '+ min + ' до ' + max;}
-      else {console.log('Упс, число не может быть меньше или равно 0');}
+min = Math.ceil(min);
+max = Math.floor(max);
+let rez=Math.floor(Math.random() * (max - min + 1)) + min
+if(min>0) {return `${ rez } целое число из диапазона от ${ min } до ${ max }`;}
+else {console.log('Упс, число не может быть меньше или равно 0');}
 };
-returnRandomInteger();
+returnRandomInteger(1,48);
 
 let checkingMaxStrLength = function (str, max) {
        if(str.length<=max){
@@ -13,4 +13,4 @@ let checkingMaxStrLength = function (str, max) {
     }
     else {console.log('false')}
 };
-checkingMaxStrLength();
+checkingMaxStrLength('Вася пьет компот',140);
