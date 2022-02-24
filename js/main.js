0831e750c8b6a1c7e577bbc7e32a6f0cdc2f449b
vertina61 <natalya.vertieva@mail.ru>
2022-02-24 18:03:04 +0300

    Исправили ошибки ESLint

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-let returnRandomInteger = function (min, max) {
-min = Math.ceil(min);
-max = Math.floor(max);
-let rez=Math.floor(Math.random() * (max - min + 1)) + min
-if(min>0) {return `${ rez } целое число из диапазона от ${ min } до ${ max }`;}
-else {console.log('Упс, число не может быть меньше или равно 0');}
+const returnRandomInteger = function (min, max) {
+  min=Math.ceil(min);
+  max=Math.floor(max);
+  const rez = Math.floor(Math.random() * (max - min + 1)) + min;
+  if(min>0) {return `${ rez } целое число из диапазона от ${ min } до ${ max }`;}
+  else {return('Упс, число не может быть меньше или равно 0');}
 };
 returnRandomInteger(1,48);
 
-let checkingMaxStrLength = function (str, max) {
-       if(str.length<=max){
-        console.log('true')
-    }
-    else {console.log('false')}
+const checkingMaxStrLength = function (str, max) {
+  if(str.length<=max){
+    return('true');}
+  else {
+    return('false');}
 };
 checkingMaxStrLength('Вася пьет компот',140);
