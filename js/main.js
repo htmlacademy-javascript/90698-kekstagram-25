3e52c9a6d82aa79e09d93b31c3b115043989970a
vertina61 <natalya.vertieva@mail.ru>
2022-02-24 11:36:12 +0300

    Переименовали функции по camelCase

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-let ReturnRandomInteger = function (min, max) {
+let returnRandomInteger = function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
        let rez=Math.floor(Math.random() * (max - min + 1)) + min
       if(min>0) {return rez + ' целое число из диапазона от '+ min + ' до ' + max;}
       else {console.log('Упс, число не может быть меньше или равно 0');}
 };
-ReturnRandomInteger();
+returnRandomInteger();
 
-let CheckingMaxStrLength = function (str, max) {
+let checkingMaxStrLength = function (str, max) {
        if(str.length<=max){
         console.log('true')
     }
     else {console.log('false')}
 };
-CheckingMaxStrLength();
+checkingMaxStrLength();
