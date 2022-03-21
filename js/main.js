8201351d5515014e92f0157fab6c803214a81631
vertina61 <natalya.vertieva@mail.ru>
2022-03-21 07:28:27 +0300

    Вывод объектов через массив

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ checkingMaxStrLength('Вася пьет компот',140);*/
   //returnRandomInteger (1,48);*/
 
 
-const CREATE_OBJ = 25;
 const returnRandomInteger = function (min, max) {
   min=Math.ceil(min);
   max=Math.floor(max);
@@ -26,14 +25,14 @@ const returnRandomInteger = function (min, max) {
 };
 
 const DESCRIPTION=['Фото1','Фото2','Фото3','Фото4','Фото5','Фото6','Фото7','Фото8','Фото9','Фото10','Фото11','Фото12','Фото13','Фото14','Фото15','Фото16','Фото17','Фото18','Фото19','Фото20','Фото21','Фото22','Фото23','Фото24','Фото25'];
-const MESSAGE=['Всё отлично! В целом всё неплохо. Но не всё.','Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.','Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.','Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const NAME=['Иван','Петр','Вася','Инна','Оля','Катя'];
+const MESSAGES=['Всё отлично! В целом всё неплохо. Но не всё.','Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.','Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.','Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const NAMES=['Иван','Петр','Вася','Инна','Оля','Катя'];
 
 const createComment = (id) => ({
   id,
   avatar: `img/avatar-${returnRandomInteger(1, 6)}.svg`,
-  message:MESSAGE[returnRandomInteger(0,MESSAGE.length-1)],
-  name:NAME[returnRandomInteger(0,NAME.length-1)]
+  message:MESSAGES[returnRandomInteger(0,MESSAGES.length-1)],
+  name:NAMES[returnRandomInteger(0,NAMES.length-1)]
 });
 
 const createObject = (id) => {
@@ -52,10 +51,13 @@ const createObject = (id) => {
   };
 };
 
-const generateObjects = (count) => {
+/*const generateObjects = (count) => {
   for (let i = 1; i <= count; i++) {
-    console.log(createObject(i));
-  }
-};
-
-generateObjects(CREATE_OBJ);
+    (createObject(i));
+  };
+};*/
+const RAZ=25;
+const generateObjects=[];
+for (let i = 1; i <= RAZ; i++){
+  generateObjects.push(createObject(i));
+}
