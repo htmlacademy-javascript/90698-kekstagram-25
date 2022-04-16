715321c8da5495dba539c1f33c5f799212b8ed5f
vertina61 <natalya.vertieva@mail.ru>
2022-04-16 13:43:03 +0300

    Создали модуль для формы

---
+++ js/form.js
@@ -0,0 +1,9 @@
+const form=document.querySelector('.img-upload__form');
+const pristine=new Pristine (form);
+form.addEventListener('submit', (evt)=>{
+    evt.preventDefault();
+    const isValid=pristine.validate();
+    if(isValid) {
+            }
+            else {}
+        });
